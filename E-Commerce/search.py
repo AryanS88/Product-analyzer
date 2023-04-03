e79778fd996d62350ea0217edb09e1884b1cb352
Anshulgada <80207612+Anshulgada@users.noreply.github.com>
2023-04-04 02:00:41 +0530

    Live share started

--- E-Commerce/search.py
+++ E-Commerce/search.py
@@ -27,9 +27,9 @@ class Seach:
         
         # title = Label(frame1, text = "Welcome to Our Family",font=("Times New Roman",20,"bold"),bg="white",fg="green").place(x= 50,y=30)
         self.search_img = ImageTk.PhotoImage(file = 'Images/search_new_small.png')
-        btn = Button(frame1,bd=0, bg = 'black', image = self.search_img).place(relx = 1.0, anchor="ne", relwidth = 0.08, relheight=1)
+        btn = Button(frame1, bd = 0, bg = 'black', image = self.search_img).place(relx = 1.0, anchor="ne", relwidth = 0.08, relheight=1)
 
-    
+
 
 
 
