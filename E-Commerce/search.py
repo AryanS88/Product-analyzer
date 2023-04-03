16f7347dd8225344a11baeb273feffb71c579b52
Anshulgada <80207612+Anshulgada@users.noreply.github.com>
2023-04-04 01:58:11 +0530

    Search Edited

--- E-Commerce/search.py
+++ E-Commerce/search.py
@@ -21,14 +21,15 @@ class Seach:
         # set the window position to the center of the screen
         root.geometry("1500x800+{}+{}".format(x, y))
         
-        # Creation of Registration Frame 
-        frame1 = Frame(self.root,bg="black")
-        frame1.place(x = 200, y = 150, width=800, height=60)
+        # Create Base Frame
+        frame1 = Frame(self.root, bg = "#fff")
+        frame1.place(x = 350, y = 200, width = 800, height = 60)
         
         # title = Label(frame1, text = "Welcome to Our Family",font=("Times New Roman",20,"bold"),bg="white",fg="green").place(x= 50,y=30)
-        
+        self.search_img = ImageTk.PhotoImage(file = 'Images/search_new_small.png')
+        btn = Button(frame1,bd=0, bg = 'black', image = self.search_img).place(relx = 1.0, anchor="ne", relwidth = 0.08, relheight=1)
 
-
+    
 
 
 
